0c01910b295ab824b46c8c5420f0d9775443cfc0
Rafao <64379110+rafao-godoy@users.noreply.github.com>
2023-09-07 22:34:23 +0000

    Inclusão do michael

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
-function Home() {
-    return <h1> Hey Wallas segue o teste dos meus estudos. =-) </h1>
+import React from 'react';
+
+const Home = () => {
+
+  return (
+
+    <div>
+
+      <h1>Hey Wallas segue o teste dos meus estudos. =-) </h1>
+          <img src="https://fmeducativa.ucdb.br/wp-content/uploads/2020/11/MICHAEL-JACKSON.jpg"/>
+
+    </div>
+  )
 }
+
 
 export default Home;
