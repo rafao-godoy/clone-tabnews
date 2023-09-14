ac68bee3f4fc58622a2c55fb2ff31c33becc8f8c
Rafao <64379110+rafao-godoy@users.noreply.github.com>
2023-09-14 23:35:04 +0000

    ajuste do `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Michael from "/workspaces/clone-tabnews/images/michael.png";
 
 const Home = () => {
   return (
     <div>
       <h1>Hey Wallas segue o teste dos meus estudos. =-) </h1>
-      <img src={Michael} />
       <img src="https://fmeducativa.ucdb.br/wp-content/uploads/2020/11/MICHAEL-JACKSON.jpg" />
     </div>
   );
