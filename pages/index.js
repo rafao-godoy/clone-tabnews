5cf87a594571fca1705134109f2c5a5ce4fed7d8
Rafao <64379110+rafao-godoy@users.noreply.github.com>
2023-09-14 23:20:26 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,14 @@
-import React from 'react';
+import React from "react";
 import Michael from "/workspaces/clone-tabnews/images/michael.png";
 
 const Home = () => {
-
   return (
-
     <div>
-
       <h1>Hey Wallas segue o teste dos meus estudos. =-) </h1>
-          <img src={Michael}/>
-          <img src="https://fmeducativa.ucdb.br/wp-content/uploads/2020/11/MICHAEL-JACKSON.jpg"/>
-
-
+      <img src={Michael} />
+      <img src="https://fmeducativa.ucdb.br/wp-content/uploads/2020/11/MICHAEL-JACKSON.jpg" />
     </div>
-  )
-}
-
+  );
+};
 
 export default Home;
